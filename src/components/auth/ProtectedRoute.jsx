f31308806e340cc96599604cf1176f29743e7870
Luis Rojas <luis.rojasco84@gmail.com>
2025-09-21 02:04:46 -0500

    Creacion e implementacion del navbar con dropdown responsive

--- src/components/auth/ProtectedRoute.jsx
+++ src/components/auth/ProtectedRoute.jsx
@@ -1,10 +1,19 @@
 import { Navigate, Outlet } from "react-router-dom"
+import DashBoardLayout from "../layout/DashBoardLayout"
 
 const ProtectedRoute = ({children}) => {
     const isAuthenticated = true
     const loading = false
+
+    if (loading) {
+        return <div>Loading...</div>
+    }
+
+    if (!isAuthenticated) {
+        return <Navigate to="/login" replace/>
+    }
   return (
-    <div>ProtectedRoute</div>
+    <DashBoardLayout>{children ? children : <Outlet/>}</DashBoardLayout>
   )
 }
 
---
+++ src/components/layout/DashBoardLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DashBoardLayout = ({children}) => {
+  return (
+    <div>DashBoardLayout
+        <div>{children}</div>
+    </div>
+  )
+}
+
+export default DashBoardLayout
