6c2ce5e872c7eb0c2b87a62eb59bc530534fd2ea
Luis Rojas <luis.rojasco84@gmail.com>
2025-09-30 16:40:44 -0500

    creacion de UI de registro de usuario

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -169,7 +169,7 @@ const Login = () => {
 
           <div>
             <label className="block text-sm font-medium text-gray-700 mb-2">
-              Password
+              Contraseña
             </label>
             <div className="relative">
               <Lock className="absolute left-4 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5" />
